9e8b60239d8e80ddddb530577ed6f241a1c23d54
Gunveer Singh <125648258+Gunveer-Singh@users.noreply.github.com>
2026-04-10 13:24:58 +0530

    Update bresenham_line.cpp

--- bresenham_line.cpp
+++ bresenham_line.cpp
@@ -13,7 +13,7 @@ int main()
     int dx = x2 - x1;
     int dy = y2 - y1;
 
-    int p = 2 * dy - dx;  
+    int p = 2*dy-dx;  
 
     int x = x1;
     int y = y1;
@@ -24,11 +24,11 @@ int main()
         x = x + 1;
 
         if(p < 0){
-            p = p + 2 * dy;
+            p = p + 2*dy;
         }
         else{
             y = y + 1;
-            p = p + 2 * dy - 2 * dx;
+            p = p + 2*dy - 2*dx;
         }
         putpixel(x, y, WHITE);
     }
